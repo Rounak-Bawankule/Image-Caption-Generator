43d4f4b31a2e661ca34ad04e827366c02bbe5d81
Rounak <brounak16@gmail.com>
2021-06-01 13:29:32 +0530

    added JS code for talk-back instead of gTTS

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template,request
 from working_code import predict_caption, encode_image
 import numpy
-from gtts import gTTS
 import os
 
 app = Flask(__name__)
@@ -16,14 +15,6 @@ def generate(path):
     caption = predict_caption(photo)
     return caption
 
-def generate_sound(caption, fname):
-    try:
-        os.remove('./static/sounds/sound.mp3')
-    except:
-        pass
-    language = 'en'
-    myobj = gTTS(text=caption, lang=language, slow=False)
-    myobj.save(f"./static/sounds/sound.mp3")
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -34,7 +25,6 @@ def homepage():
         path = f'./static/user_images/{f.filename}'
         f.save(path)
         caption = generate(path)
-        generate_sound(caption, f.filename)
         return render_template('index.html', caption=caption, path=path)
     else:
         return render_template('index.html')
@@ -44,5 +34,5 @@ def contact():
     return render_template("contact.html")
 
 
-if '__name__' == '__main__':
-    app.run(debug=True, port=8000)
+# if '__name__' == '__main__':
+app.run(debug=True, port=8000)
